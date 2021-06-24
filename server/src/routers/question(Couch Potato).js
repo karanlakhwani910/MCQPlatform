799214979e0fa9accc579cf247ab3dd2e5455896
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-06-25 00:16:03 +0530

    testing

--- server/src/routers/question(Couch Potato).js
+++ server/src/routers/question(Couch Potato).js
@@ -238,9 +238,9 @@ router.post("/login", async (req, res) => {
     // await user.save();
     const date=new Date();
     console.log("current date is",date);
-    const prevDate=new Date(2021, 5, 24, 18, 35, 00, 0);
+    const prevDate=new Date(2021, 5, 24, 18, 45, 00, 0);
     console.log("prev date is",prevDate)
-    const nextDate=new Date(2021, 5, 24, 18, 40, 00, 0);
+    const nextDate=new Date(2021, 5, 24, 19, 00, 00, 0);
     console.log("current compared to prev",prevDate<date,nextDate<date);
     
     if(!(date>prevDate&&date<nextDate))
