0b21a14e44a26f41b1227e7c2200df26c1949f2a
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-06-25 10:23:09 +0530

    final slot testing

--- server/src/routers/question(Xenatus).js
+++ server/src/routers/question(Xenatus).js
@@ -277,12 +277,20 @@ router.post("/login", async (req, res) => {
     //   });
     // await user.save();
     //10.00 -10.30
+    // const date=new Date();
+    // console.log("current date is",date.toLocaleString());
+    // const prevDate=new Date(2021, 5, 25, 16, 30, 00, 0);
+    // console.log("prev date is",prevDate.toLocaleString())
+    // const nextDate=new Date(2021, 5, 25, 17, 00, 00, 0);
+    // console.log("next date is",nextDate.toLocaleString())
+
     const date=new Date();
     console.log("current date is",date.toLocaleString());
-    const prevDate=new Date(2021, 5, 25, 16, 30, 00, 0);
+    const prevDate=new Date(2021, 5, 25, 5, 00, 00, 0);
     console.log("prev date is",prevDate.toLocaleString())
-    const nextDate=new Date(2021, 5, 25, 17, 00, 00, 0);
+    const nextDate=new Date(2021, 5, 25, 5, 30, 00, 0);
     console.log("next date is",nextDate.toLocaleString())
+
     // console.log("current compared to prev",prevDate<date,nextDate>date);
     
     if(!(date>prevDate&&date<nextDate))
