dca24ab486313e79a955f783ccfe157ce53dc7a9
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-06-26 19:53:44 +0530

    xenatus final slot testing for round 2

--- server/src/models/main-site-user.js
+++ server/src/models/main-site-user.js
@@ -82,7 +82,7 @@ const MainSiteUserSchema = new mongoose.Schema(
 const circuitronMainSiteUser = mongoose.model("main-site-user", MainSiteUserSchema);
 const couchPotatoMainSiteUser=conn.couchPotato.model("main-site-user",MainSiteUserSchema);
 const xenatusMainSiteUser=conn.xenatus.model("main-site-user",MainSiteUserSchema);
-const xenatusMainSiteUser2=conn.xenatus.model("main-site-user-2",MainSiteUserSchema);
+const xenatusMainSiteUser2=conn.xenatus.model("main-site-users-2",MainSiteUserSchema);
 const c2cMainSiteUser=conn.c2c.model("main-site-user",MainSiteUserSchema);
 
 module.exports = {couchPotatoMainSiteUser,circuitronMainSiteUser,xenatusMainSiteUser,xenatusMainSiteUser2,c2cMainSiteUser}
