3c74b0a82c9605dde4b63131c3d78d3b1bcb138b
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-06-26 23:31:57 +0530

    xenatus final backend-time set

--- server/src/routers/question(Xenatus).js
+++ server/src/routers/question(Xenatus).js
@@ -283,7 +283,7 @@ router.post("/login", async (req, res) => {
     console.log("current date is",date.toLocaleString());
     const prevDate=new Date(2021, 5, 26, 17, 00, 00, 0);
     console.log("prev date is",prevDate.toLocaleString())
-    const nextDate=new Date(2021, 5, 26, 18, 00, 00, 0);
+    const nextDate=new Date(2021, 5, 26, 18, 05, 00, 0);
     console.log("next date is",nextDate.toLocaleString())
 
 
